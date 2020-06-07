b7647431a17229680db0c7435a183b04b8433258
Isaac Bustamante <izaack.97@gmail.com>
2020-06-07 13:24:35 -0700

    Se agrego el componente noticias y noticia

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaTopHeadLines } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class NoticiasService {
   constructor(private http: HttpClient) { }
 
   getTopHeadLines(){
-    return this.http.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=8c8aceec7cf54a7dbb424dbfda9c34c8`);
+    return this.http.get<RespuestaTopHeadLines>(`http://newsapi.org/v2/top-headlines?country=us&apiKey=8c8aceec7cf54a7dbb424dbfda9c34c8`);
   }
 }
