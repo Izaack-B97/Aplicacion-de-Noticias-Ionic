e3a41dc995bf341748951efe9e5da28de4cececc
Isaac Bustamante <izaack.97@gmail.com>
2020-06-15 14:02:42 -0700

    action-sheet para opciones"

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() i: number;
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(private iab: InAppBrowser, private actionSheetCtlr: ActionSheetController) { }
 
   ngOnInit() {}
 
@@ -22,4 +23,39 @@ export class NoticiaComponent implements OnInit {
     const browser = this.iab.create(this.noticia.url, '_system'); // Lo abre en el navegador nativo del sistema
   }
 
+  lanzarMenu(){
+    this.presentActionSheet();
+  }
+
+  // Declaramos el Action Sheet
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtlr.create({
+      header: 'Opciones',
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share-social-outline',
+          handler: () => {
+            console.log('Compartir');
+          }
+        },
+        {
+          text: 'Favorito',
+          icon: 'heart',
+          handler: () => {
+            console.log('Favoritos');
+          }
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
