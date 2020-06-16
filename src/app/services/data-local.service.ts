bbab3b500168414dd353a533bbe9f1d45f0e02b1
Isaac Bustamante <izaack.97@gmail.com>
2020-06-16 14:49:41 -0700

    Se añadio animaciones y quedo los toast

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Article } from '../interfaces/interfaces';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,17 @@ import { Article } from '../interfaces/interfaces';
 export class DataLocalService {
   noticias: Article[] = [];
 
-  constructor(private storage: Storage) { }
+  constructor(private storage: Storage, private toastController: ToastController) { }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 1500,
+      color: 'primary',
+      position: 'top'
+    });
+    toast.present();
+  }
 
   // Proceso para guardar noticias
   guardarNoticias(noticia: Article){
@@ -17,6 +28,9 @@ export class DataLocalService {
     if (!existe){
       this.noticias.unshift(noticia); // Lo pone al principio del arreglo
       this.storage.set('favoritos', this.noticias);
+      this.presentToast('Agregado a favoritos');
+    } else {
+      this.presentToast('Ya existe en favoritos');
     }
   }
 
@@ -33,5 +47,6 @@ export class DataLocalService {
   borrarNoticia(noticia: Article){
     this.noticias = this.noticias.filter(notices => notices.title !== noticia.title);
     this.storage.set('favoritos', this.noticias);
+    this.presentToast('Borrado de favoritos');
   }
 }
